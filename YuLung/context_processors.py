09e1432dd0316b45448f166b5bdf49024c96986c
Fuyuan Cheng <gloomcheng@gmail.com>
2018-04-23 01:37:21 +0800

    feat: add BASE_URL variable
    Add static variable of BASE_URL for fetch site domain

--- YuLung/context_processors.py
+++ YuLung/context_processors.py
@@ -1,6 +1,8 @@
+from django.conf import settings
 import itertools
 
 def static_vars(request):
     return {
         'site_mail': 'tour@yulon-motor.com.tw',
+        'base_url': settings.BASE_URL
     }
